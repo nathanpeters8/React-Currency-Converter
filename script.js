40b78b9ac1f31659642b20a4aed9bb690491670e
Nathan Peters <peters.nate8@gmail.com>
2023-12-13 14:03:55 -0700

    remade app to incorporate useState hook

--- script.js
+++ script.js
@@ -1,40 +1,23 @@
 // Currency Converter
 
-// Child Component for USD and EURO input fields
-class CurrencyInput extends React.Component {
-  render() { 
-    const { value, handleChange } = this.props;
-    
-    return <input value={value} onChange={handleChange} type='number'/>;
-  }
-}
-
-// Parent Currency Converter Component
-class CurrencyConverter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      rate: 0.89,
-      usd: 1,
-      euro: 1 * 0.89,
-    };
-
-    this.handleUsdChange = this.handleUsdChange.bind(this);
-    this.handleEuroChange = this.handleEuroChange.bind(this);
-  }
+const {useState, useEffect} = React;
+const CurrencyConverter = () =>  {
+  const [rate, setRate] = useState(0.89);
+  const [usd, setUSD] = useState(1);
+  const [euro, setEuro] = useState(1 * 0.89);
 
   // convert euro to USD
-  toUsd(amount, rate) {
+  const toUsd = (amount, rate) => {
     return amount * (1 / rate);
   }
 
   // convert USD to euro
-  toEuro(amount, rate) {
+  const toEuro = (amount, rate) => {
     return amount * rate;
   }
 
   // return conversion value if input is a number
-  convert(amount, rate, equation) {
+  const convert = (amount, rate, equation) => {
     const input = parseFloat(amount);
     if (Number.isNaN(input)) {
       return '';
@@ -43,45 +26,38 @@ class CurrencyConverter extends React.Component {
   }
 
   // update state when USD input is changed
-  handleUsdChange(event) {
-    const euro = this.convert(event.target.value, this.state.rate, this.toEuro);
+  const handleUsdChange = (event) => {
+    const euro = convert(event.target.value, rate, toEuro);
 
-    this.setState({
-      usd: event.target.value,
-      euro
-    })
+    setUSD(event.target.value);
+    setEuro(euro);
   }
 
   // update state when Euro input is changed
-  handleEuroChange(event) {
-    const usd = this.convert(event.target.value, this.state.rate, this.toUsd);
+  const handleEuroChange = (event) => {
+    const usd = convert(event.target.value, rate, toUsd);
 
-    this.setState({
-      euro: event.target.value,
-      usd,
-    });
+    setEuro(event.target.value);
+    setUSD(usd);
   }
 
-  render() {
-    const { rate, usd, euro } = this.state;
-    return (
-      <div className='container'>
-        <div className='text-center p-3 mb-2'>
-          <h2 className='mb-2 border-bottom'>Currency Converter</h2>
-          <h4>USD 1 : {rate} EURO</h4>
-        </div>
-        <div className='row text-center'>
-          <div className='col-12'>
-            <span className='mr-1'>USD</span>
-            <CurrencyInput value={usd} handleChange={this.handleUsdChange} />
-            <span className='mx-3'>=</span>
-            <CurrencyInput value={euro} handleChange={this.handleEuroChange} />
-            <span className='ml-1'>EURO</span>
-          </div>
+  return (
+    <div className='container'>
+      <div className='text-center p-3 mb-2'>
+        <h2 className='mb-2 border-bottom'>Currency Converter</h2>
+        <h4>USD 1 : {rate} EURO</h4>
+      </div>
+      <div className='row text-center'>
+        <div className='col-12'>
+          <span className='mr-1'>USD</span>
+          <input value={usd} onChange={handleUsdChange} type='number'/>       
+          <span className='mx-3'>=</span>
+          <input value={euro} onChange={handleEuroChange} type='number' />
+          <span className='ml-1'>EURO</span>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 const container = document.getElementById('root');
